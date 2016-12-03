a0ebf29586ce63eb0815b5012c5405016b97d694
Mohammed Sohail <sohailsameja@gmail.com>
2016-12-03 22:37:01 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Status](https://051610e8.ngrok.io/.svg)](https://medium.com/p/f222ece09c23)
+
+## Status Badge Service
+
+Click on the badge above to read more!
---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+const url = 'http://127.0.0.1:5000';
+
+function status(req, res) {
+       http.get(url, function (response) {
+            // Its Online
+            res.writeHead(200, {
+                'Content-Type': 'image/svg+xml'
+            });
+            fs.createReadStream('./badges/online.svg').pipe(res);
+        }).on('error', function(e) {
+            // Its Offline
+            res.writeHead(200, {
+                'Content-Type': 'image/svg+xml'
+            });
+            fs.createReadStream('./badges/offline.svg').pipe(res);            
+    });
+};
+
+http.createServer(status).listen(port);
